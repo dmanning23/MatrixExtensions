bf835e781892571c2795af3402b95079be58c981
Dan Manning <dmanning23@hotmail.com>
2016-04-07 23:17:29 -0500

    update nuget

--- MatrixExtensions/buildNugetPackages.ps1
+++ MatrixExtensions/buildNugetPackages.ps1
@@ -1,3 +1,3 @@
 nuget pack .\MatrixExtensions.Android\MatrixExtensions.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-
-nuget pack .\MatrixExtensions.WindowsGL\MatrixExtensions.WindowsGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\MatrixExtensions.iOS\MatrixExtensions.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\MatrixExtensions.DesktopGL\MatrixExtensions.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
